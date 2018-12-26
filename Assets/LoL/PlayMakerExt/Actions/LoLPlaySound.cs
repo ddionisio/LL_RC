c2b7eb659718a7e839c3386528577c820cb0a965
ddionisio <garapata@gmail.com>
2018-12-26 14:13:14 -0500

    added playmaker ext. for lol

---
+++ Assets/LoL/PlayMakerExt/Actions/LoLPlaySound.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions.LoL {
+    [ActionCategory("Legends of Learning")]
+    public class LoLPlaySound : FsmStateAction {
+        public FsmString path;
+        public FsmBool isBackground;
+        public FsmBool isLoop;
+
+        public override void Reset() {
+            path = null;
+            isBackground = false;
+            isLoop = false;
+        }
+
+        public override void OnEnter() {
+            if(LoLManager.isInstantiated)
+                LoLManager.instance.PlaySound(path.Value, isBackground.Value, isLoop.Value);
+
+            Finish();
+        }
+    }
+}
---
+++ Assets/LoL/PlayMakerExt/Actions/LoLStopSound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions.LoL {
+    [ActionCategory("Legends of Learning")]
+    public class LoLStopSound : FsmStateAction {
+        public FsmString path;
+
+        public override void Reset() {
+            path = null;
+        }
+
+        public override void OnEnter() {
+            if(LoLManager.isInstantiated)
+                LoLManager.instance.StopSound(path.Value);
+
+            Finish();
+        }
+    }
+}
