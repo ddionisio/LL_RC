ea6f2938e3987d06df2927576755db9f4b414458
ddionisio <garapata@gmail.com>
2019-01-06 17:24:00 -0500

    some tests

---
+++ Assets/Scripts/Game/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public M8.RigidBodyController2D bodyControl;
+    public M8.ForceController2D gravityControl;
+
+    public void InputHorizontalPressed(M8.InputAction action) {
+
+    }
+
+    public void InputHorizontalReleased(M8.InputAction action) {
+
+    }
+
+    public void InputActPressed(M8.InputAction action) {
+
+    }
+
+    public void InputActReleased(M8.InputAction action) {
+
+    }
+}
