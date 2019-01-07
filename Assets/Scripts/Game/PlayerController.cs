c335cb71c21eaa0ba09d6c73fbfc9cec1b2b9b6f
ddionisio <garapata@gmail.com>
2019-01-07 18:23:51 -0500

    more testing movement

--- Assets/Scripts/Game/PlayerController.cs
+++ Assets/Scripts/Game/PlayerController.cs
@@ -2,24 +2,84 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
+    public enum JumpState {
+        None,
+        JumpStart,
+        Jump
+    }
+
     public M8.RigidBodyController2D bodyControl;
     public M8.ForceController2D gravityControl;
 
-    public void InputHorizontalPressed(M8.InputAction action) {
+    [Header("Move")]
+    public float standMoveFalloffDelay = 0.3f;
+    public float jumpForce = 20f;
 
+    [Header("Input")]
+    public M8.InputAction moveHorzInput;
+    public M8.InputAction actInput;
+
+    public bool actIsEnabled { get; private set; }
+    public Vector2 jumpDir { get; private set; }
+
+    private float mMoveHorz;
+    private JumpState mJumpState;
+
+    void OnEnable() {
+        actIsEnabled = false;
+        jumpDir = bodyControl.dirHolder.up;
+
+        mMoveHorz = 0f;
+
+        if(bodyControl) {
+            if(mJumpState != JumpState.None)
+                bodyControl.lockDragCounter--;
+        }
+
+        mJumpState = JumpState.None;
     }
 
-    public void InputHorizontalReleased(M8.InputAction action) {
+    void Update() {
+        //movement
+        mMoveHorz = moveHorzInput.GetAxis();
 
+        //determine if we can act
+        if(bodyControl.isGrounded) {
+            actIsEnabled = true;
+            jumpDir = bodyControl.dirHolder.up;
+        }
+        else {
+            //check if we are on contact of a side
+            //determine actDir
+            actIsEnabled = false;
+        }
+
+        if(actIsEnabled) {
+            if(mJumpState == JumpState.None) {
+                var actState = actInput.GetButtonState();
+                if(actState == M8.InputAction.ButtonState.Pressed) {
+                    mJumpState = JumpState.JumpStart;
+                }
+            }
+        }
     }
 
-    public void InputActPressed(M8.InputAction action) {
+    void FixedUpdate() {
+        //update body control
+        bodyControl.moveHorizontal = mMoveHorz;
 
-    }
-
-    public void InputActReleased(M8.InputAction action) {
-
+        switch(mJumpState) {
+            case JumpState.JumpStart:
+                bodyControl.lockDragCounter++;
+                bodyControl.body.drag = 0f;
+                mJumpState = JumpState.Jump;
+                break;
+            case JumpState.Jump:
+                bodyControl.body.AddForce(jumpDir * jumpForce, ForceMode2D.Impulse);
+                bodyControl.lockDragCounter--;
+                mJumpState = JumpState.None;
+                break;
+        }
     }
 }
