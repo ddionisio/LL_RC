ff116851f6a364f2fc17112b717d22782cb81cf8
ddionisio <garapata@gmail.com>
2019-01-25 09:57:52 -0500

    offscreen goal display, setup placeholder extrusive rocks, setup sedimentary interaction

---
+++ Assets/Scripts/Actions/OffscreenIndicatorActions.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions.Game {
+    [ActionCategory("Game")]
+    public class OffscreenIndicatorSetPosition : ComponentAction<OffscreenIndicatorPosition> {
+        [RequiredField]
+        [ObjectType(typeof(OffscreenIndicatorPosition))]
+        public FsmOwnerDefault gameObject;
+
+        public FsmOwnerDefault target;
+
+        public override void Reset() {
+            gameObject = null;
+            target = null;
+        }
+
+        public override void OnEnter() {
+            var go = Fsm.GetOwnerDefaultTarget(gameObject);
+            if(UpdateCache(go)) {
+                var targetGO = Fsm.GetOwnerDefaultTarget(target);
+                if(targetGO)
+                    cachedComponent.targetPosition = targetGO.transform.position;
+            }
+
+            Finish();
+        }
+    }
+
+    [ActionCategory("Game")]
+    public class OffscreenIndicatorSetActive : ComponentAction<OffscreenIndicatorPosition> {
+        [RequiredField]
+        [ObjectType(typeof(OffscreenIndicatorPosition))]
+        public FsmOwnerDefault gameObject;
+
+        public FsmBool active;
+
+        public override void Reset() {
+            gameObject = null;
+            active = false;
+        }
+
+        public override void OnEnter() {
+            var go = Fsm.GetOwnerDefaultTarget(gameObject);
+            if(UpdateCache(go)) {
+                cachedComponent.targetActive = active.Value;
+            }
+
+            Finish();
+        }
+    }
+}
---
+++ Assets/Scripts/Game/OffscreenIndicatorPosition.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OffscreenIndicatorPosition : MonoBehaviour {
+    public Vector3 targetPosition { get; set; }
+
+    [SerializeField]
+    bool _targetActive;
+
+    public bool targetActive { get { return _targetActive; } set { _targetActive = value; } }
+
+    [M8.TagSelector]
+    public string cameraTag = ""; //grab from this if no direct camera
+    [SerializeField]
+    Camera _camera = null;
+
+    public Transform displayRoot;
+        
+    public Camera cameraSource {
+        get {
+            if(!_camera && !string.IsNullOrEmpty(cameraTag)) {
+                var go = GameObject.FindGameObjectWithTag(cameraTag);
+                if(go)
+                    _camera = go.GetComponent<Camera>();
+            }
+
+            return _camera;
+        }
+
+        set { _camera = value; }
+    }
+
+    void Awake() {
+        displayRoot.gameObject.SetActive(false);
+    }
+
+    void Update() {
+        if(!cameraSource) return;
+        if(!targetActive) return;
+
+        Vector3 vp = cameraSource.WorldToViewportPoint(targetPosition);
+
+        bool isEdge = false;
+
+        if(vp.x > 1) {
+            vp.x = 1; isEdge = true;
+        }
+        else if(vp.x < 0) {
+            vp.x = 0; isEdge = true;
+        }
+
+        if(vp.y > 1) {
+            vp.y = 1; isEdge = true;
+        }
+        else if(vp.y < 0) {
+            vp.y = 0; isEdge = true;
+        }
+
+        if(isEdge) {
+            displayRoot.gameObject.SetActive(true);
+
+            Vector3 pos = cameraSource.ViewportToWorldPoint(vp);
+            displayRoot.position = new Vector3(pos.x, pos.y, 0f);
+            displayRoot.up = ((Vector2)(displayRoot.position - cameraSource.transform.position)).normalized;// new Vector3(vp.x - 0.5f, vp.y - 0.5f, 0.0f);
+        }
+        else {
+            displayRoot.gameObject.SetActive(false);
+        }
+    }
+}
