19b47d4e393316d2a47c67316f62795baecdedc2
ddionisio <garapata@gmail.com>
2019-04-01 19:15:01 -0400

    level 1&2 tile, mineral block tile/animation

--- Assets/Scripts/Game/Collect.cs
+++ Assets/Scripts/Game/Collect.cs
@@ -2,19 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class Collect : MonoBehaviour {
+    [Header("Box Edit")]
+    public Vector2 size;
+    public Vector2 sizeSpriteBaseOfs;
+    public Vector2 sizeColliderExtraOfs;
+
+    public SpriteRenderer[] spriteBases;
+    public SpriteRenderer spriteFrame;
+
+    public BoxCollider2D colliderExtra;
+
     [Header("Info")]
     public int amount = 1;
 
     [Header("Data")]
     [M8.TagSelector]
     public string playerTag = "Player";
-
-    public PlayMakerFSM fsm;
-    public string fsmCollectEvent = "COLLECT";
-
+    
+    [Header("Display")]
     public GameObject collectGO;
     public GameObject collectedGO;
+
+    [Header("Animation")]
+    public M8.Animator.Animate animator;
+    [M8.Animator.TakeSelector(animatorField = "animator")]
+    public string takeCollect;
     
     [Header("Signals")]
     public SignalInteger signalCollect;
@@ -22,15 +36,21 @@ public class Collect : MonoBehaviour {
     public bool isCollected { get; private set; }
 
     private Collider2D mCollider;
-        
+
+    void OnEnable() {
+        if(Application.isPlaying) {
+            if(collectGO) collectGO.SetActive(!isCollected);
+            if(collectedGO) collectedGO.SetActive(isCollected);
+        }
+    }
+
     void Awake() {
-        if(!fsm)
-            fsm = GetComponent<PlayMakerFSM>();
+        if(Application.isPlaying) {
+            if(collectGO) collectGO.SetActive(true);
+            if(collectedGO) collectedGO.SetActive(false);
 
-        if(collectGO) collectGO.SetActive(true);
-        if(collectedGO) collectedGO.SetActive(false);
-
-        mCollider = GetComponent<Collider2D>();
+            mCollider = GetComponent<Collider2D>();
+        }
     }
 
     void OnTriggerEnter2D(Collider2D collision) {
@@ -62,18 +82,53 @@ public class Collect : MonoBehaviour {
         }
     }
 
+#if UNITY_EDITOR
+    void Update() {
+        if(!Application.isPlaying) {
+            if(!mCollider)
+                mCollider = GetComponent<Collider2D>();
+
+            var boxCollider = mCollider as BoxCollider2D;
+            if(boxCollider)
+                boxCollider.size = size;
+
+            if(spriteBases != null) {
+                for(int i = 0; i < spriteBases.Length; i++) {
+                    if(spriteBases[i])
+                        spriteBases[i].size = size + sizeSpriteBaseOfs;
+                }
+            }
+
+            if(spriteFrame)
+                spriteFrame.size = size;
+
+            if(colliderExtra)
+                colliderExtra.size = size + sizeColliderExtraOfs;
+        }
+    }
+#endif
+
     void Collected() {
         isCollected = true;
 
         if(mCollider)
             mCollider.enabled = false;
 
+        signalCollect.Invoke(amount);
+
+        //update display
+        if(animator && !string.IsNullOrEmpty(takeCollect))
+            StartCoroutine(DoCollectedAnimate());
+        else {
+            if(collectGO) collectGO.SetActive(false);
+            if(collectedGO) collectedGO.SetActive(true);
+        }
+    }
+
+    IEnumerator DoCollectedAnimate() {
+        yield return animator.PlayWait(takeCollect);
+
         if(collectGO) collectGO.SetActive(false);
         if(collectedGO) collectedGO.SetActive(true);
-
-        if(fsm && !string.IsNullOrEmpty(fsmCollectEvent))
-            fsm.SendEvent(fsmCollectEvent);
-
-        signalCollect.Invoke(amount);
     }
 }
