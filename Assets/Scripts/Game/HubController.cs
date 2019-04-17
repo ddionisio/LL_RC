7f40916071389b131cbd297fdd1dd99a6395b095
David Dionisio <ddionisio@renegadeware.com>
2019-04-17 16:07:47 -0400

    rock cycle ui, show arrows to indicate where to go in hub

---
+++ Assets/Scripts/Game/HubController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HubController : GameModeController<HubController> {
+
+    public InventoryData inventory;
+
+    protected override IEnumerator Start() {
+        yield return base.Start();
+
+        //fail-safe
+        var curProgress = LoLManager.instance.curProgress;
+        switch(curProgress) {
+            case 1:
+            case 3:
+            case 5:
+                //add some minerals
+                if(inventory.mineralsCount == 0) {
+                    for(int i = 0; i < inventory.minerals.Length; i++) {
+                        if(inventory.minerals[i])
+                            inventory.minerals[i].count += 1;
+                    }
+                }
+                break;
+            case 7:
+                //add some organics
+                if(inventory.organicsCount == 0) {
+                    for(int i = 0; i < inventory.organics.Length; i++) {
+                        if(inventory.organics[i])
+                            inventory.organics[i].count += 5;
+                    }
+                }
+                break;
+            case 9:
+                //add some minerals
+                if(inventory.mineralsCount == 0) {
+                    for(int i = 0; i < inventory.minerals.Length; i++) {
+                        if(inventory.minerals[i])
+                            inventory.minerals[i].count += 1;
+                    }
+                }
+                //add some organics
+                if(inventory.organicsCount == 0) {
+                    for(int i = 0; i < inventory.organics.Length; i++) {
+                        if(inventory.organics[i])
+                            inventory.organics[i].count += 5;
+                    }
+                }
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Modals/ModalRockCycle.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModalRockCycle : M8.ModalController, M8.IModalPush, M8.IModalActive {
+    public const string parmCycleIndex = "index";
+
+    [System.Serializable]
+    public class Group {
+        public M8.UI.Graphics.ColorPulse[] highlights;
+
+        public void SetActive(bool active) {
+            for(int i = 0; i < highlights.Length; i++) {
+                if(highlights[i])
+                    highlights[i].enabled = active;
+            }
+        }
+    }
+
+    public Group[] groups;
+
+    void M8.IModalActive.SetActive(bool aActive) {
+        if(aActive) {
+
+        }
+    }
+
+    void M8.IModalPush.Push(M8.GenericParams parms) {
+        int index = -1;
+
+        if(parms != null) {
+            if(parms.ContainsKey(parmCycleIndex))
+                index = parms.GetValue<int>(parmCycleIndex);
+        }
+
+        for(int i = 0; i < groups.Length; i++)
+            groups[i].SetActive(i == index);
+    }
+}
